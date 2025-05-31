495b6085958c19d8a91368f9e6c33d734b729f77
larrygelberg <bebopdaddy@gmail.com>
2025-05-31 11:39:23 -0400

    added debug capabilities and starting to add shape file

--- ws.py
+++ ws.py
@@ -1,6 +1,7 @@
 import sys
 import string
 import random
+import argparse
 from collections import Counter
 
 # Function to print a list of words in four columns
@@ -12,35 +13,42 @@ def pretty_print(mylist):
         mylist.append("")
     print('\n'.join("%-16s %-16s %-16s %s" % (mylist[i], mylist[i + len(mylist) // 4], mylist[i + len(mylist) * 2 // 4], mylist[i + len(mylist) * 3 // 4]) for i in range(len(mylist) // 4)))
 
+# Function to fill in 'empty' spaces with '.' for debugging
+def debug_fill():
+    for i in range(width):
+        for j in range(height):
+            if (final_game[i][j] == 0):
+                final_game[i][j] = '.'
+
 # Function to fill in 'empty' spaces with random characters
-def simple_fill(g):
-    for i in range(height):
-        for j in range(width):
-            if (g[i][j] == 0):
-                g[i][j] = random.choice(string.ascii_uppercase)
+def simple_fill():
+    for i in range(width):
+        for j in range(height):
+            if (final_game[i][j] == 0):
+                final_game[i][j] = random.choice(string.ascii_uppercase)
 
 # Function to fill in 'empty' spaces with sneaky characters based on prevalence of letters in the word set
-def hairy_fill(g, words):
+def hairy_fill():
     # First, build a histogram of frequency of letters in the word set
     all_characters = ''.join(words)
     character_count = Counter(all_characters)
     most_common = character_count.most_common(7)
     most_frequent_chars = [char for char, count in most_common]
-    # print (most_frequent_chars)
+    if debug:
+        print (most_frequent_chars)
 
     # fill in the empty spaces based on the most frequent letters
-    for i in range(height):
-        for j in range(width):
-            if (g[i][j] == 0):
-                #g[i][j] = random.choice(string.ascii_uppercase)
-                g[i][j] = random.choice(most_frequent_chars)
+    for i in range(width):
+        for j in range(height):
+            if (final_game[i][j] == 0):
+                final_game[i][j] = random.choice(most_frequent_chars)
 
 # Function to print the game board
-def print_game(g):
+def print_game():
     for i in range(height):
         print ('')
         for j in range(width):
-            print(str(g[j][i]),end=' ')
+            print(str(final_game[j][i]),end=' ')
 
 # Function to read the words from a file
 def read_words(fname):
@@ -67,13 +75,12 @@ def insert(word, direction):
     for attempt in range (0, attempt_limit):
         x = random.randint(0,width-1)
         y = random.randint(0,height-1)
-        #forward = random.choice([True, False])
-        #horizontal = random.choice([True,False])
-        #diagonal = random.choice([True,False])
+
         forward = True if direction & 1 else False
         horizontal = True if direction & 2  else False
         diagonal = True if direction & 4 else False
-        #print attempt, ": Trying", word,"at",x, y
+        if debug:
+            print(attempt, ": Trying", word,"at",x, y)
 
         if diagonal:
             # Horizontal and Vertical words
@@ -156,43 +163,70 @@ def insert(word, direction):
         used.append(word)
     else:
         unused.append(word)
+
+# Function to set the board at the beginning of each iteration
+def initialize_board(game, shape):
+    k = 0
+    if shape:
+        game = [[ shape[k] for i in range(height)] for j in range(width)]
+    else:
+        game = [[0 for i in range(height)] for j in range(width)]
+
         
 ################################################################
 
-width = 10
-height = 10
 final_game = []
 final_unused = []
 final_used = []
-easy = True
+game = []
+shape = []
 
-if (len(sys.argv) < 2):
-    print("usage: %s word_file (width height easy|hard)" % sys.argv[0]) 
-    sys.exit()
+# Create the parser
+parser = argparse.ArgumentParser(description='Process some files and options.')
 
-if (len(sys.argv) == 2):
-    width = int(input("Enter width: "))
-    height = int(input("Enter height: "))
-elif (len(sys.argv) >= 4):
-    width = int(sys.argv[2])
-    height = int(sys.argv[3])
+# Required argument
+parser.add_argument('words_file', type=str, help='The words file')
 
-if (len(sys.argv) == 5 and sys.argv[4] == "hard" ):
-    easy = False
+# Optional arguments
+parser.add_argument('--width', type=int, help='Optional width')
+parser.add_argument('--height', type=int, help='Optional height')
+parser.add_argument('--hard', action='store_true', help='Optional hard flag')
+parser.add_argument('--shape_file', type=str, help='Optional shape file')
+parser.add_argument('--debug', action='store_true', help='Debug Flag')
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Access the arguments
+width = 10 if args.width is None else args.width
+height = 10 if args.height is None else args.height
+easy = False if args.hard else True
+debug = True if args.debug else False
+if args.shape_file is not None:
+    try:
+        with open(args.shape_file, 'r') as file:
+            content = file.read()
+            # Convert the content to a list of characters
+            shape = list(content)
+    except FileNotFoundError:
+        print(f"Error: The file '{args.shape_file}' was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 # read the word file
-words = read_words(sys.argv[1])
-#print words
+words = read_words(args.words_file)
+if debug:
+    print(words)
 
 # loop over placements trying to find the most optimal board
 game_attempt_limit = 200
 min_placed_words = len(words)
-
 for g in range(0,game_attempt_limit):
     unused = []
     used = []
     # initialize the game board
-    game = [[0 for i in range(width)] for j in range(height)]
+    game = [[0 for i in range(height)] for j in range(width)]
+    #initialize_board(game, shape)
 
     # stick words into the game board
     for word in words:
@@ -200,7 +234,8 @@ for g in range(0,game_attempt_limit):
         insert(word, direction)
 
     placed_words = len(unused)
-    #print "Board",g,"has",placed_words,"words left."
+    if debug:
+        print("Board",g,"has",placed_words,"words left.")
 
     if placed_words == 0:
         final_game = game
@@ -215,13 +250,15 @@ for g in range(0,game_attempt_limit):
         min_placed_words = placed_words
 
 # fill in remaining spaces with random letters
-if easy:
-    simple_fill(final_game)
+if debug:
+    debug_fill()
+elif easy:
+    simple_fill()
 else:
-    hairy_fill(final_game, words)
+    hairy_fill()
 
 # print the results
-print_game(final_game)
+print_game()
 
 print()
 print()
@@ -229,10 +266,10 @@ print("Find these words: ")
 pretty_print(sorted(final_used, key=len))
 
 # print any words that couldn't be placed
-if len(unused) > 0:
+if debug and len(unused) > 0:
     print()
     print("Unused:",final_unused)
-    print
+    print()
 
 
 
